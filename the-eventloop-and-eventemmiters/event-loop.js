441f110c2e5702e13cdf8086de41a73aff28d370
Gospel011 <ugochukwugospeli111@gmail.com>
2023-08-09 18:52:01 +0100

    Did some experiments on the nodejs event loop. I discovered that process.env.UV_THREADPOOL_SIZE is undefined for windows and should be manually set in an npm script like so: set UV_THREADPOOL_SIZE=custom_size & node app.js

--- the-eventloop-and-eventemmiters/event-loop.js
+++ the-eventloop-and-eventemmiters/event-loop.js
@@ -1,1 +1,58 @@
-// ./../complete-node-bootcamp\2-how-node-works\starter\test-file.txt
+// ./../complete-node-bootcamp/2-how-node-works/starter/test-file.txt
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+
+console.log('Hi from top-level code');
+
+setTimeout(()=> {
+    const date = Date.now()
+
+    // Timer
+    setTimeout(() => {
+        console.log('3 seconds timer finished');
+    }, 0);
+
+    // I/O task
+    const text = fs.readFile('./../complete-node-bootcamp/2-how-node-works/starter/test-file.txt', 'utf-8', (data, err) => {
+        console.log('finishen I/O file read task');
+    });
+
+    // setImmediate callback
+    setImmediate(()=>{
+        console.log('setImmediate callback finished');
+    })
+
+    //process.nextTick
+    process.nextTick(()=>{
+        console.log('process.nextTick callback called');
+    });
+
+    //password encryption which is a more involved task that would be offloaded to the threadpool
+    crypto.pbkdf2('secret_password', 'salt', 100000, 200, 'sha512', (err, derivedKey) => {
+        console.log((Date.now() - date)/1000, `Encrypted password = ${derivedKey}`);
+    });
+
+
+    crypto.pbkdf2('secret_password', 'salt', 100000, 200, 'sha512', (err, derivedKey) => {
+        console.log((Date.now() - date)/1000, `Encrypted password = ${derivedKey}`);
+    });
+
+
+    crypto.pbkdf2('secret_password', 'salt', 100000, 200, 'sha512', (err, derivedKey) => {
+        console.log((Date.now() - date)/1000, `Encrypted password = ${derivedKey}`);
+    });
+
+    
+    crypto.pbkdf2('secret_password', 'salt', 100000, 200, 'sha512', (err, derivedKey) => {
+        console.log((Date.now() - date)/1000, `Encrypted password = ${derivedKey}`);
+    });
+
+
+    crypto.pbkdf2('secret_password', 'salt', 100000, 200, 'sha512', (err, derivedKey) => {
+        console.log((Date.now() - date)/1000, `Encrypted password = ${derivedKey}`);
+    });
+
+
+}, 0);
