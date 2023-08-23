449c62f4b93ee6bb92ab22a877ea0232e07f6b95
Gospel011 <ugochukwugospeli111@gmail.com>
2023-08-23 18:09:53 +0100

    Created my own middleware that adds a requestTime property to the req object. I also installed the thirdparty middleware called morgan

--- express_api_project/app.js
+++ express_api_project/app.js
@@ -5,48 +5,192 @@ const { rejects } = require('assert');
 const app = express();
 app.use(express.json());
 
+// Top level constants
 const portNumber = 8000;
 
-const tours = JSON.parse(
-  fs.readFileSync(
-    `${__dirname}/../complete-node-bootcamp/4-natours/starter/dev-data/data/tours-simple.json`,
-    'utf-8'
-  )
-);
+const toursPath = `${__dirname}/../complete-node-bootcamp/4-natours/starter/dev-data/data/tours-simple.json`;
+const tours = JSON.parse(fs.readFileSync(toursPath, 'utf-8'));
 
-const asyncWriteFile = async (filePath, text) => {
-  return Promise((resolve, reject) => {
-    fs.writeFile(filePath, text, (err) => {
-      err
-        ? reject('The file was not written successfully')
-        : resolve('File written successfully');
-    });
+// Middleware functions
+const addTime = (req, res, next) => {
+  const date = new Date()
+
+  req['requestTime'] = `${date.toDateString()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  console.log(req.requestTime)
+  next();
+};
+
+// My custom middlewares
+app.use(addTime);
+
+// Functions
+const getTour = (req) => {
+  const id = req.params.id * 1;
+  console.log(`id = ${id}`);
+  const tour = tours.find((el) => el.id === id);
+  return tour;
+};
+
+const updateToursFile = (
+  callback,
+  path = toursPath,
+  new_tour = JSON.stringify(tours)
+) => {
+  fs.writeFile(path, new_tour, (err) => {
+    callback(err);
   });
 };
 
-//CREATE--Post requests
-app.post('/api/vi/tours', (req, res) => {
-  new_tour = req.body;
+// Request handler functions
+const createNewTour = (req, res) => {
+  const new_id = tours.length === 0 ? 0 : tours[tours.length - 1].id + 1;
+
+  console.log(new_id);
+  const new_tour = req.body.hasOwnProperty('id')
+    ? Object.assign(req.body, { id: new_id })
+    : Object.assign({ id: new_id }, req.body);
+
+  console.log(`bool = ${req.body.hasOwnProperty('id')}`);
+
   console.log(tours.push(new_tour));
   fs.writeFile(
     `./complete-node-bootcamp/4-natours/starter/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
-      err ? console.log(err.message) : console.log('File written successfully');
+      if (err) {
+        console.log(err.message);
+      } else {
+        res.status(201);
+        res.json({
+          status: 'New tour created',
+          statusCode: 201,
+        });
+        console.log('File written successfully');
+      }
     }
   );
-  res.send('Done');
-});
+};
 
-// READ--Get requests
-app.get('/api/vi/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200);
   res.json({
     status: 'success',
+    statusCode: 200,
     results: tours.length,
     body: {
       tours: tours,
     },
+  });
+};
+
+const getTourById = (req, res) => {
+  console.log('Get tour by id');
+  const tour = getTour(req);
+  console.log(tour);
+
+  if (tour === undefined) {
+    res.status(404);
+    res.json({
+      status: 'Failed',
+      statusCode: 404,
+      message: 'No tour found for that id.',
+    });
+  }
+  res.status(200);
+  res.json({
+    status: 'Success',
+    statusCode: 200,
+    body: {
+      tour: tour,
+    },
+  });
+};
+
+const updateTourById = (req, res) => {
+  const id = req.params.id * 1;
+  const tourIndex = tours.findIndex((el) => el.id === id);
+  let tour = tours.find((el) => el.id === id);
+  tour = Object.assign(tour, req.body);
+  tours[tourIndex] = tour;
+
+  fs.writeFile(
+    `./complete-node-bootcamp/4-natours/starter/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    'utf-8',
+    (err) => {
+      if (err) {
+        console.log(err.message);
+        res.status(500);
+        res.json({
+          status: 'Failed',
+          statusCode: 500,
+          message:
+            "Something happened, so your request couldn't be completed successfully",
+        });
+      } else {
+        res.status(200);
+        res.json({
+          status: 'Success',
+          statusCode: 200,
+          message: 'Tour updated successfully',
+          requestTime: req.requestTime,
+          body: {
+            tour: tour,
+          },
+        });
+      }
+    }
+  );
+};
+
+// Routes or Endpoints
+
+//CREATE--Post requests
+// 1. Create new tour
+app.route('/api/v1/tours').post(createNewTour);
+
+// READ--Get requests
+//  1. Get all tours
+//  2. Get tour from id
+app.route('/api/v1/tours').get(getAllTours);
+
+// PATCH--Update tour by id
+app.route('/api/v1/tours/:id').patch(updateTourById).get(getTourById);
+
+// DELETE--Delete tour by id
+app.route('/api/v1/tours/:id').delete((req, res) => {
+  const tour = getTour(req);
+
+  if (tour === undefined) {
+    res.status(404);
+    res.json({
+      status: 'Failed',
+      statusCode: 404,
+      message: 'No tour found for that id.',
+    });
+  } else {
+    tours.splice(tours.indexOf(tour), 1);
+  }
+
+  updateToursFile((err) => {
+    if (err) {
+      res.status(500);
+      res.json({
+        status: 'Failed',
+        statusCode: 500,
+        message: 'Something happened, tour was not successfully deleted',
+      });
+    } else {
+      res.status(200);
+      res.json({
+        status: 'Success',
+        statusCode: 200,
+        message: 'Tour deleted',
+        body: {
+          tour: null,
+        },
+      });
+    }
   });
 });
 
---
+++ express_api_project/test.js
@@ -0,0 +1,5 @@
+const num = [1, 2, 3, 4, 5]
+
+num.splice(1, 1)
+
+console.log(num)
